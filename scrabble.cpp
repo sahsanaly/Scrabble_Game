ac8a3507a7a56d81b8f5b1d6c844adde67580d08
Mitchell Reid <mitchellreid02@gmail.com>
2022-04-30 18:17:25 +1000

    Implement easy features for the main menu

--- scrabble.cpp
+++ scrabble.cpp
@@ -3,13 +3,70 @@
 
 #include <iostream>
 
-#define EXIT_SUCCESS    0
+#define EXIT_SUCCESS 0
 
-int main(void) {
-   LinkedList* list = new LinkedList();
+int main(void)
+{
+   LinkedList *list = new LinkedList();
    delete list;
 
-   std::cout << "TODO: Implement Scrabble!" << std::endl;
+   std::cout << "Welcome to Scrabble!" << std::endl;
+   std::cout << "-------------------" << std::endl;
+
+   bool terminate = false;
+   while (!terminate)
+   {
+      std::cout
+          << "Menu" << std::endl
+          << "----" << std::endl
+          << "1. New Game" << std::endl
+          << "2. Load Game" << std::endl
+          << "3. Credits (Show student information)" << std::endl
+          << "4. Quit" << std::endl;
+
+      int userInput;
+      std::cin >> userInput;
+      std::cout << std::endl;
+
+      if (userInput == 1)
+      {
+         // Create a new game
+         std::cout << "Not yet implemented!" << std::endl;
+      }
+      if (userInput == 2)
+      {
+         // Load a game
+         std::cout << "Not yet implemented!" << std::endl;
+      }
+      if (userInput == 3)
+      {
+         std::cout
+             << "--------------------------------------" << std::endl
+             << "Name: Syed Ali" << std::endl
+             << "Student ID: s3736294" << std::endl
+             << "Email: s3736294@student.rmit.edu.au" << std::endl
+             << std::endl
+             << "Name: Mitchell Reid" << std::endl
+             << "Student ID: s3901557" << std::endl
+             << "Email: s3901557@student.rmit.edu.au" << std::endl
+             << std::endl
+             << "Name: Tsz Him Matthew Cheung" << std::endl
+             << "Student ID: s3902085" << std::endl
+             << "Email: s3902085@student.rmit.edu.au" << std::endl
+             << std::endl
+             << "Name: Spencer Morrison" << std::endl
+             << "Student ID: s3906025" << std::endl
+             << "Email: s3906025@student.rmit.edu.au" << std::endl
+             << "--------------------------------------" << std::endl;
+      }
+      if (userInput == 4)
+      {
+         std::cout << "Goodbye" << std::endl;
+         terminate = true;
+      }
+
+      std::cout << std::endl;
+   }
 
    return EXIT_SUCCESS;
 }
