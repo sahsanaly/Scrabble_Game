0280bc8b5c2f78d72fcb58320db3372616008962
Mitchell Reid <mitchellreid02@gmail.com>
2022-05-02 15:56:28 +1000

    Create Scrabble Definition

---
+++ Scrabble.h
@@ -0,0 +1,41 @@
+#include <string>
+
+class Player;
+class Board;
+class Bag;
+
+class Scrabble
+{
+public:
+    // Default loader
+    Scrabble();
+    // Save game loader
+    Scrabble(std::string saveFileName);
+    // Main game loop.
+    // Loops until either:
+    //      A player wins
+    //      The players quit early
+    //      The game is saved to a file
+    void mainLoop();
+
+private:
+    Player player1;
+    Player player2;
+    Board board;
+    Bag bag;
+
+    // Parse the inital command
+    // Get additional commands until place done is received
+    // Check the placed tiles to ensure they meet placing rules
+    // Put tiles onto the board if they meet placing rules
+    // If the tiles breach placing rules, return false, otherwise return true
+    bool placeTile(std::string initialCommand);
+
+    // Check if the tile indicated is valid
+    // If yes, replace it and return true
+    // If no, return false
+    bool replaceTile(std::string initialCommand);
+
+    // Save the game to indicated save file
+    bool saveGame(std::string initialCommand);
+}
