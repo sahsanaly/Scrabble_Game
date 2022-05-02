f751c6ba81b1946dc6f2ab4bf54ddaac7e93b44f
morrsp <s3906025@student.rmit.edu.au>
2022-05-02 16:24:01 +1000

    Finishes player header. Co-authored-by: s3902085 <s3902085@student.rmit.edu.au>

--- Player.h
+++ Player.h
@@ -11,16 +11,24 @@ public:
    Player();
    Player(Player &other);
    ~Player();
-
+// Given a certain tile, adds tile to hand.
    bool drawTile(Tile* tile);
-   bool placeTile(char letter);
+// Places a certain tile from the hand.
+   Tile* placeTile(char letter);
 
    Hand* getHand();
-
+// Prints the hand for reading.
    void readHand();
+// Adds to the player's score.
+   bool addPoints();
+// Return the player's score.
+   int* getScore();
+
+   std::string getAsString();
 
 private:
     Hand* hand;
+    int score;
 };
 
 #endif // ASSIGN2_PLAYER_H
