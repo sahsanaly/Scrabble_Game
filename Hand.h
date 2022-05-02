30a25d99c95ff15114275edfea45ee942c845e48
s3902085 <s3902085@student.rmit.edu.au>
2022-05-02 15:36:55 +1000

    Implements hand header Co-Authored-By: Spencer Morrison <s3906025@student.rmit.edu.au>

--- Hand.h
+++ Hand.h
@@ -1,5 +1,25 @@
 
-#ifndef ASSIGN2_NODE_H
-#define ASSIGN2_NODE_H
+#ifndef ASSIGN2_HAND_H
+#define ASSIGN2_HAND_H
 
 #include "LinkedList.h"
+
+class Hand
+{
+public:
+   Hand();
+   Hand(Hand &other);
+   ~Hand();
+
+   // Add specific tile to hand
+   bool addTile(Tile* tile);
+   // Remove tile from hand
+   bool removeTile(Tile* tile);
+   
+   // Find the first tile with the desired letter and return its pointer. Return NULL if not found.
+   Tile* getTile(char letter);
+
+   LinkedList tilesInHand;
+};
+
+#endif //ASSIGN2_HAND_H
