ce2bac1d05ac75ca74265807b248ab7964800e5d
s3902085 <s3902085@student.rmit.edu.au>
2022-05-02 15:54:04 +1000

    Implements Bag header Co-authored-by: morrsp <morrsp@users.noreply.github.com>

--- Bag.h
+++ Bag.h
@@ -1,3 +1,30 @@
 
-#ifndef ASSIGN2_NODE_H
-#define ASSIGN2_NODE_H
+#ifndef ASSIGN2_BAG_H
+#define ASSIGN2_BAG_H
+
+#include "LinkedList.h"
+#include <iostream>
+
+class Bag
+{
+public:
+   Bag();
+   Bag(Bag &other);
+   ~Bag();
+
+   // Add specific tile to the tail of the Bag
+   bool addTile(Tile* tile);
+   
+   // Grab the head tile.
+   Tile* drawTile();
+
+   // Shuffle the bag (by default during initiation)
+   bool shuffle();
+
+   // Convert Bag to string
+   std::string convertToString();
+
+   LinkedList tilesInBag;
+};
+
+#endif
