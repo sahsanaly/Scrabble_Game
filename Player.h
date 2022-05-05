46436e2a17e96d70a1228a323ac5f5ce8f304559
Mitchell Reid <mitchellreid02@gmail.com>
2022-05-05 23:27:55 +1000

    Add a name to player

--- Player.h
+++ Player.h
@@ -8,27 +8,28 @@
 class Player
 {
 public:
-   Player();
+   Player(std::string name);
    Player(Player &other);
    ~Player();
-// Given a certain tile, adds tile to hand.
-   bool drawTile(Tile* tile);
-// Places a certain tile from the hand.
-   Tile* placeTile(char letter);
+   // Given a certain tile, adds tile to hand.
+   bool drawTile(Tile *tile);
+   // Places a certain tile from the hand.
+   Tile *placeTile(char letter);
 
-   Hand* getHand();
-// Prints the hand for reading.
+   Hand *getHand();
+   // Prints the hand for reading.
    void readHand();
-// Adds to the player's score.
+   // Adds to the player's score.
    bool addPoints();
-// Return the player's score.
-   int* getScore();
+   // Return the player's score.
+   int *getScore();
 
    std::string getAsString();
 
 private:
-    Hand* hand;
-    int score;
+   Hand *hand;
+   int score;
+   std::string name;
 };
 
 #endif // ASSIGN2_PLAYER_H
