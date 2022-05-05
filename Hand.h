f4c4d0d33ebd56432094cbc5c2ec9cb2c680a75f
s3902085 <s3902085@student.rmit.edu.au>
2022-05-05 19:16:10 +1000

    Readabilty changes

--- Hand.h
+++ Hand.h
@@ -20,12 +20,13 @@ public:
    // Find the first tile with the desired letter and return its pointer. Return NULL if not found.
    std::shared_ptr<Tile> getTile(Letter letter);
 
-   // Convert Hand to string of tiles and return it
-   std::string getAsString();
-
    // Return all tiles in this hand.
    LinkedList getTilesInHand();
+
+   // Convert Hand to String of tiles and return it
+   std::string getAsString();
    
+   // Print the String format of the Hand.
    void print();
 
 private:
