54d20d0495a93e3d51b7c2c2b8aad1eb699e44aa
s3902085 <s3902085@student.rmit.edu.au>
2022-05-06 22:10:04 +1000

    Initial (untested) implementation

--- Player.cpp
+++ Player.cpp
@@ -5,6 +5,64 @@
 Player::Player(std::string name)
 {
     this->name = name;
+    this->score = 0;
+    this->hand = std::make_shared<Hand>();
 }
 
-Player::~Player() {}
+Player::Player(Player &other)
+{
+    this->name = other.name;
+    this->score = other.score;
+    this->hand = std::make_shared<Hand>(other.hand);
+}
+
+Player::~Player() 
+{
+    // Not necessary due to use of smart pointers
+}
+
+void Player::drawTile(std::shared_ptr<Tile> tile)
+{
+    this->hand->addTile(tile);
+}
+
+std::shared_ptr<Tile> Player::placeTile(char letter)
+{
+    std::shared_ptr<Tile> tileToPlace = this->hand->getTile(letter);
+
+    this->hand->removeTile(tileToPlace);
+
+    return tileToPlace;
+}
+
+std::shared_ptr<Hand> Player::getHand()
+{
+    return this->hand;
+}
+
+int Player::getScore()
+{
+    return this->score;
+}
+
+void Player::addScore(int numScoreToAdd)
+{
+    this->score = score + numScoreToAdd;
+}
+
+void Player::printHand()
+{
+    this->hand->print();
+}
+
+std::string Player::getAsString()
+{
+    std::string playerAsString = "";
+    playerAsString.append(name);
+    playerAsString.append("\n");
+    playerAsString.append(std::to_string(score));
+    playerAsString.append("\n");
+    playerAsString.append(this->hand->getAsString());
+    
+    return playerAsString;
+}
